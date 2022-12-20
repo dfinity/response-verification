5bdf02d701d4cef11ae7a9ce296142717e2f6334
Nathan Mc Grath <contact@nathanos.dev>
2022-12-20 18:00:55 +0100

    feat: add cel to ast parser

--- cel-parser/src/lib.rs
+++ cel-parser/src/lib.rs
@@ -1,31 +1,12 @@
 #![allow(dead_code, unused_variables, unused_imports, unused_imports)]
 
 mod mock;
+mod model;
+mod parser;
+
+pub use model::*;
+
 use mock::*;
-
-struct RequestCertification {
-    certified_request_headers: Vec<String>,
-    certified_query_parameters: Vec<String>,
-}
-
-enum ResponseCertification {
-    HeaderExclusions(Vec<String>),
-    CertifiedHeaders(Vec<String>),
-}
-
-struct Certification {
-    request_certification: Option<RequestCertification>,
-    response_certification: ResponseCertification,
-}
-
-impl Certification {
-    fn new() -> Self {
-        Certification {
-            request_certification: None,
-            response_certification: ResponseCertification::HeaderExclusions(vec![]),
-        }
-    }
-}
 
 #[cfg(test)]
 mod tests {
