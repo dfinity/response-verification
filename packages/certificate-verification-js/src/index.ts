b311d2a13e7a306bf5dbbf7b1ec1e3ae4009b7ec
NathanosDev <contact@nathanos.dev>
2023-06-28 13:44:19 +0200

    feat: add certificate verification JS library
    re #TT-253

---
+++ packages/certificate-verification-js/src/index.ts
@@ -0,0 +1,82 @@
+import {
+  Cbor,
+  Certificate,
+  HashTree,
+  reconstruct,
+  compare,
+} from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
+import { PipeArrayBuffer, lebDecode } from '@dfinity/candid';
+import { CertificateTimeError, CertificateVerificationError } from './error';
+
+export async function verifyCertification(
+  canisterId: Principal,
+  encodedCertificate: ArrayBuffer,
+  encodedTree: ArrayBuffer,
+  rootKey: ArrayBuffer,
+  maxCertificateTimeOffsetMs: number,
+): Promise<HashTree> {
+  const certificate = await Certificate.create({
+    certificate: encodedCertificate,
+    canisterId,
+    rootKey,
+  });
+  const tree = Cbor.decode<HashTree>(encodedTree);
+
+  validateCertificateTime(certificate, maxCertificateTimeOffsetMs);
+  validateTree(tree, certificate, canisterId);
+
+  return tree;
+}
+
+function validateCertificateTime(
+  certificate: Certificate,
+  maxCertificateTimeOffsetMs: number,
+): void {
+  const certificateTimeNs = lebDecode(
+    new PipeArrayBuffer(certificate.lookup(['time'])),
+  );
+  const certificateTimeMs = Number(certificateTimeNs / BigInt(1_000_000));
+  const nowMs = Date.now();
+
+  if (certificateTimeMs - maxCertificateTimeOffsetMs > nowMs) {
+    throw new CertificateTimeError(
+      `Invalid certificate: time ${certificateTimeMs} is too far in the future (current time: ${nowMs})`,
+    );
+  }
+
+  if (certificateTimeMs + maxCertificateTimeOffsetMs < nowMs) {
+    throw new CertificateTimeError(
+      `Invalid certificate: time ${certificateTimeMs} is too far in the past (current time: ${nowMs})`,
+    );
+  }
+}
+
+async function validateTree(
+  tree: HashTree,
+  certificate: Certificate,
+  canisterId: Principal,
+): Promise<void> {
+  const treeRootHash = await reconstruct(tree);
+  const certifiedData = certificate.lookup([
+    'canister',
+    canisterId.toUint8Array(),
+    'certified_data',
+  ]);
+
+  if (!certifiedData) {
+    throw new CertificateVerificationError(
+      'Could not find certified data in the certificate.',
+    );
+  }
+
+  if (!equal(certifiedData, treeRootHash)) {
+    throw new CertificateVerificationError(
+      'Tree root hash did not match the certified data in the certificate.',
+    );
+  }
+}
+
+function equal(a: ArrayBuffer, b: ArrayBuffer): boolean {
+  return compare(a, b) === 0;
+}
