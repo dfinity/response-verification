a46f9ba32fffc292ad5c6143c68e4215519191ba
Kepler Vital <mail@keplervital.com>
2023-07-14 14:52:06 +0200

    test: Update agent-js used for tests to the latest version

--- packages/ic-response-verification-tests/README.md
+++ packages/ic-response-verification-tests/README.md
@@ -5,3 +5,5 @@
 ```shell
 ./scripts/e2e.sh
 ```
+
+> Tests should be run with nodejs 18, nodejs 19+ has a known issue with node-fetch (https://github.com/node-fetch/node-fetch/issues/1735)
--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -1,4 +1,4 @@
-SDK_GIT_BRANCH="response-verification-v2"
+SDK_GIT_BRANCH="update-replica-again"
 
 # Download the SDK repo so we can build and test against the latest changes
 download_sdk_repo() {
