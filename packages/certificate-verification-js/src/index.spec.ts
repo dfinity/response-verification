58b36eb5da47f15b1019aa4bb545dd85e3b0a6a3
NathanosDev <contact@nathanos.dev>
2023-07-21 10:55:25 +0200

    test: add unit tests for certificate verification JS library
    re #TT-296

--- packages/certificate-verification-js/src/index.spec.ts
+++ packages/certificate-verification-js/src/index.spec.ts
@@ -7,43 +7,147 @@ import { verifyCertification } from './index';
 
 globalThis.crypto = webcrypto as Crypto;
 
-describe('verifyCertification', () => {
-  it('should verify a valid certificate with delegation', async () => {
-    const userId = '1234';
+describe('verifyCertification', async () => {
+  const userId = '1234';
 
-    const username = 'testuser';
-    const usernameHash = new Uint8Array(
-      createHash('sha256').update(username).digest(),
-    );
+  const username = 'testuser';
+  const usernameHash = new Uint8Array(
+    createHash('sha256').update(username).digest(),
+  );
 
-    const hashTree: HashTree = [
-      2,
-      new Uint8Array(Buffer.from(userId)),
-      [3, usernameHash],
-    ];
-    const rootHash = await reconstruct(hashTree);
-    const cborEncodedTree = Cbor.encode(hashTree);
+  const hashTree: HashTree = [
+    2,
+    new Uint8Array(Buffer.from(userId)),
+    [3, usernameHash],
+  ];
+  const rootHash = await reconstruct(hashTree);
+  const cborEncodedTree = Cbor.encode(hashTree);
 
-    const canisterId = Principal.fromUint8Array(
-      new Uint8Array([0, 0, 0, 0, 0, 0, 0, 1]),
-    );
-    const time = BigInt(Date.now());
+  const canisterId = Principal.fromUint8Array(
+    new Uint8Array([0, 0, 0, 0, 0, 0, 0, 1]),
+  );
+  const time = BigInt(Date.now());
+  const MAX_CERT_TIME_OFFSET_MS = 5000;
 
-    const certificate = new CertificateBuilder(
-      canisterId.toString(),
-      new Uint8Array(rootHash),
-    )
-      .withTime(time)
-      .withDelegation(123n, [{ high: 10, low: 0 }])
-      .build();
+  it.each([
+    {
+      scenario: 'with a valid signature',
+    },
+    {
+      withDelegation: true,
+      scenario: 'with a delegation',
+    },
+    {
+      timeOverride: BigInt(Date.now() - 1000),
+      scenario: 'with a time in the past',
+    },
+    {
+      withDelegation: true,
+      timeOverride: BigInt(Date.now() - 1000),
+      scenario: 'with a delegation and a time in the past',
+    },
+    {
+      timeOverride: BigInt(Date.now() + 1000),
+      scenario: 'with a time in the future',
+    },
+    {
+      withDelegation: true,
+      timeOverride: BigInt(Date.now() + 1000),
+      scenario: 'with a delegation and a time in the future',
+    },
+  ])(
+    'should verify a certificate $scenario',
+    async ({ withDelegation, timeOverride }) => {
+      let certificateBuilder = new CertificateBuilder(
+        canisterId.toString(),
+        new Uint8Array(rootHash),
+      ).withTime(timeOverride ?? time);
 
-    const decodedHashTree = await verifyCertification({
-      canisterId,
-      encodedCertificate: certificate.cborEncodedCertificate,
-      encodedTree: cborEncodedTree,
-      maxCertificateTimeOffsetMs: 5000,
-      rootKey: certificate.rootKey,
-    });
-    expect(decodedHashTree).toEqual(hashTree);
-  });
+      if (withDelegation) {
+        certificateBuilder = certificateBuilder.withDelegation(123n, [
+          { high: 10, low: 0 },
+        ]);
+      }
+
+      const certificate = certificateBuilder.build();
+
+      const decodedHashTree = await verifyCertification({
+        canisterId,
+        encodedCertificate: certificate.cborEncodedCertificate,
+        encodedTree: cborEncodedTree,
+        maxCertificateTimeOffsetMs: MAX_CERT_TIME_OFFSET_MS,
+        rootKey: certificate.rootKey,
+      });
+      expect(decodedHashTree).toEqual(hashTree);
+    },
+  );
+
+  it.each([
+    {
+      withInvalidSignature: true,
+      scenario: 'with an invalid signature',
+    },
+    {
+      withInvalidSignature: true,
+      withDelegation: true,
+      scenario: 'with a delegation and an invalid signature',
+    },
+    {
+      timeOverride: BigInt(Date.now() - MAX_CERT_TIME_OFFSET_MS - 1000),
+      scenario: 'with a time too far in the past',
+    },
+    {
+      withDelegation: true,
+      timeOverride: BigInt(Date.now() - MAX_CERT_TIME_OFFSET_MS - 1000),
+      scenario: 'with a delegation a time too far in the past',
+    },
+    {
+      timeOverride: BigInt(Date.now() + MAX_CERT_TIME_OFFSET_MS + 1000),
+      scenario: 'with a time too far in the future',
+    },
+    {
+      withDelegation: true,
+      timeOverride: BigInt(Date.now() + MAX_CERT_TIME_OFFSET_MS + 1000),
+      scenario: 'with a delegation a time too far in the future',
+    },
+    {
+      rootHashOverride: new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).buffer,
+      scenario: 'with a root hash mismatch',
+    },
+  ])(
+    'should fail to verify a certificate $scenario',
+    async ({
+      withDelegation,
+      withInvalidSignature,
+      timeOverride,
+      rootHashOverride,
+    }) => {
+      let certificateBuilder = new CertificateBuilder(
+        canisterId.toString(),
+        new Uint8Array(rootHashOverride ?? rootHash),
+      ).withTime(timeOverride ?? time);
+
+      if (withDelegation) {
+        certificateBuilder = certificateBuilder.withDelegation(123n, [
+          { high: 10, low: 0 },
+        ]);
+      }
+
+      if (withInvalidSignature) {
+        certificateBuilder = certificateBuilder.withInvalidSignature();
+      }
+
+      const certificate = certificateBuilder.build();
+
+      await expect(() =>
+        verifyCertification({
+          canisterId,
+          encodedCertificate: certificate.cborEncodedCertificate,
+          encodedTree: cborEncodedTree,
+          maxCertificateTimeOffsetMs: 5000,
+          rootKey: certificate.rootKey,
+        }),
+      ).rejects.toThrowError();
+    },
+  );
 });
--- packages/ic-certification-test-utils-wasm/src/lib.rs
+++ packages/ic-certification-test-utils-wasm/src/lib.rs
@@ -1,3 +1,5 @@
+use wasm_bindgen::prelude::*;
+
 pub mod certificate_builder;
 
 mod certificate;
@@ -5,3 +7,10 @@ mod encoding;
 mod error;
 mod signature;
 mod tree;
+
+#[wasm_bindgen(start)]
+pub fn main() {
+    console_error_panic_hook::set_once();
+    log::set_logger(&wasm_bindgen_console_logger::DEFAULT_LOGGER).unwrap();
+    log::set_max_level(log::LevelFilter::Info);
+}
