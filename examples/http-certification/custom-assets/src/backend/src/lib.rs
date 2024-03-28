5160c84e76a0046f91884060949924e834781467
NathanosDev <contact@nathanos.dev>
2024-03-28 20:09:03 +0000

    docs: security fixes for custom asset canister example

--- examples/http-certification/custom-assets/src/backend/src/lib.rs
+++ examples/http-certification/custom-assets/src/backend/src/lib.rs
@@ -59,6 +59,7 @@ struct CertifiedHttpResponse<'a> {
 
 thread_local! {
     static HTTP_TREE: RefCell<HttpCertificationTree> = RefCell::new(HttpCertificationTree::default());
+    static ENCODED_RESPONSES: RefCell<HashMap<(String, String), CertifiedHttpResponse<'static>>> = RefCell::new(HashMap::new());
     static RESPONSES: RefCell<HashMap<String, CertifiedHttpResponse<'static>>> = RefCell::new(HashMap::new());
     static CEL_EXPRS: RefCell<HashMap<String, (DefaultResponseOnlyCelExpression<'static>, String)>> = RefCell::new(HashMap::new());
 }
@@ -77,13 +78,8 @@ lazy_static! {
 
 fn prepare_cel_exprs() {
     let asset_cel_expr_def = DefaultCelBuilder::response_only_certification()
-        .with_response_certification(DefaultResponseCertification::certified_response_headers(
-            vec![
-                "content-type",
-                "content-length",
-                "content-encoding",
-                "cache-control",
-            ],
+        .with_response_certification(DefaultResponseCertification::response_header_exclusions(
+            vec![],
         ))
         .build();
 
@@ -135,7 +131,7 @@ fn certify_asset_glob(glob: &str, content_type: &str) {
         .unwrap()
         .map(|entry| entry.as_file().unwrap())
     {
-        // compute the different paths we need for this asset
+        // compute the different paths needed for this asset
         let asset_file_path = identity_file.path().to_str().unwrap().to_string();
         let asset_req_path = if !asset_file_path.starts_with("/") {
             format!("/{}", asset_file_path)
@@ -203,16 +199,45 @@ fn certify_asset_with_encoding(
     // check if the file exists before certifying it
     if let Some(file) = ASSETS_DIR.get_file(format!("{}.{}", asset_file_path, encoding)) {
         let body = file.contents();
+
         // add the content encoding header
         let mut headers = vec![("content-encoding".to_string(), encoding.to_string())];
         headers.extend(additional_headers);
 
-        certify_asset_response(
-            body,
-            headers,
-            asset_tree_path,
-            format!("{}.{}", asset_req_path, encoding),
-        );
+        CEL_EXPRS.with_borrow(|cel_exprs| {
+            // get the relevant CEL expression
+            let (cel_expr_def, cel_expr_str) = cel_exprs.get(*ASSET_CEL_EXPR_PATH).unwrap();
+
+            // create the response
+            let response = create_asset_response(headers, body, cel_expr_str.to_string());
+
+            // certify the response
+            let certification =
+                HttpCertification::response_only(cel_expr_def, &response.clone().into(), None)
+                    .unwrap();
+
+            ENCODED_RESPONSES.with_borrow_mut(|responses| {
+                // store the response for later retrieval
+                responses.insert(
+                    (asset_req_path, encoding.to_string()),
+                    CertifiedHttpResponse {
+                        response,
+                        certification: certification.clone(),
+                    },
+                );
+            });
+
+            HTTP_TREE.with_borrow_mut(|http_tree| {
+                // add the certification to the certification tree
+                http_tree.insert(&HttpCertificationTreeEntry::new(
+                    asset_tree_path,
+                    &certification,
+                ));
+
+                // set the canister's certified data
+                set_certified_data(&http_tree.root_hash());
+            });
+        });
     };
 }
 
@@ -227,22 +252,8 @@ fn certify_asset_response(
         // get the relevant CEL expression
         let (cel_expr_def, cel_expr_str) = cel_exprs.get(*ASSET_CEL_EXPR_PATH).unwrap();
 
-        // set up our default headers and include additional headers provided by the caller
-        let mut headers = vec![
-            ("content-length".to_string(), body.len().to_string()),
-            (
-                IC_CERTIFICATE_EXPRESSION_HEADER.to_string(),
-                cel_expr_str.to_string(),
-            ),
-        ];
-        headers.extend(additional_headers);
-
         // create the response
-        let response = HttpAssetResponse {
-            status_code: 200,
-            headers,
-            body: Cow::Borrowed(body),
-        };
+        let response = create_asset_response(additional_headers, body, cel_expr_str.to_string());
 
         // certify the response
         let certification =
@@ -277,7 +288,8 @@ fn asset_handler(req: &HttpRequest) -> HttpResponse {
     let req_path = req.get_path().expect("Failed to get req path");
 
     RESPONSES.with_borrow(|responses| {
-        let (asset_req_path, asset_tree_path, identity_response) =
+        ENCODED_RESPONSES.with_borrow(|encoded_responses| {
+            let (asset_req_path, asset_tree_path, identity_response) =
             // if the requested path matches a static asset, serve that
             if let Some(identity_response) = responses.get(&req_path) {
                 (
@@ -294,50 +306,54 @@ fn asset_handler(req: &HttpRequest) -> HttpResponse {
                 )
             };
 
-        // extract the content encoding header
-        let content_encoding = req.headers.iter().find_map(|(name, value)| {
-            if name.to_lowercase() == "accept-encoding" {
-                Some(value)
-            } else {
-                None
-            }
-        });
-
-        let CertifiedHttpResponse {
-            certification,
-            response,
-        } = content_encoding
-            .and_then(|encoding| {
-                // if the request asks for Brotli and it's available for this file, serve that version
-                if encoding.contains("br") {
-                    if let Some(br_response) = responses.get(&format!("{}.br", asset_req_path)) {
-                        return Some(br_response);
-                    }
+            // extract the content encoding header
+            let content_encoding = req.headers.iter().find_map(|(name, value)| {
+                if name.to_lowercase() == "accept-encoding" {
+                    Some(value)
+                } else {
+                    None
                 }
+            });
 
-                // if the request asks for Gzip and it's available for this file, serve that version
-                if encoding.contains("gzip") {
-                    if let Some(gzip_response) = responses.get(&format!("{}.gzip", asset_req_path))
-                    {
-                        return Some(gzip_response);
+            let CertifiedHttpResponse {
+                certification,
+                response,
+            } = content_encoding
+                .and_then(|encoding| {
+                    // if the request asks for Brotli and it's available for this file, serve that version
+                    if encoding.contains("br") {
+                        if let Some(br_response) =
+                            encoded_responses.get(&(asset_req_path.clone(), "br".to_string()))
+                        {
+                            return Some(br_response);
+                        }
                     }
-                }
 
-                None
-            })
-            // otherwise serve the identity version
-            .unwrap_or(identity_response);
+                    // if the request asks for Gzip and it's available for this file, serve that version
+                    if encoding.contains("gzip") {
+                        if let Some(gzip_response) =
+                            encoded_responses.get(&(asset_req_path, "gzip".to_string()))
+                        {
+                            return Some(gzip_response);
+                        }
+                    }
 
-        let mut response: HttpResponse = response.clone().into();
+                    None
+                })
+                // otherwise serve the identity version
+                .unwrap_or(identity_response);
 
-        add_certificate_header(
-            &mut response,
-            &HttpCertificationTreeEntry::new(&asset_tree_path, certification),
-            &req_path,
-            &asset_tree_path.to_expr_path(),
-        );
+            let mut response: HttpResponse = response.clone().into();
 
-        response
+            add_certificate_header(
+                &mut response,
+                &HttpCertificationTreeEntry::new(&asset_tree_path, certification),
+                &req_path,
+                &asset_tree_path.to_expr_path(),
+            );
+
+            response
+        })
     })
 }
 
@@ -365,6 +381,33 @@ fn add_certificate_header(
             BASE64.encode(expr_path)
         ),
     ));
+}
+
+fn create_asset_response(
+    additional_headers: Vec<HeaderField>,
+    body: &[u8],
+    cel_expr: String,
+) -> HttpAssetResponse {
+    // set up the default headers and include additional headers provided by the caller
+    let mut headers = vec![
+        ("strict-transport-security".to_string(), "max-age=31536000; includeSubDomains".to_string()),
+        ("x-frame-options".to_string(), "DENY".to_string()),
+        ("x-content-type-options".to_string(), "nosniff".to_string()),
+        ("content-security-policy".to_string(), "default-src 'self'; form-action 'self'; object-src 'none'; frame-ancestors 'none'; upgrade-insecure-requests; block-all-mixed-content".to_string()),
+        ("referrer-policy".to_string(), "no-referrer".to_string()),
+        ("permissions-policy".to_string(), "accelerometer=(),ambient-light-sensor=(),autoplay=(),battery=(),camera=(),display-capture=(),document-domain=(),encrypted-media=(),fullscreen=(),gamepad=(),geolocation=(),gyroscope=(),layout-animations=(self),legacy-image-formats=(self),magnetometer=(),microphone=(),midi=(),oversized-images=(self),payment=(),picture-in-picture=(),publickey-credentials-get=(),speaker-selection=(),sync-xhr=(self),unoptimized-images=(self),unsized-media=(self),usb=(),screen-wake-lock=(),web-share=(),xr-spatial-tracking=()".to_string()),
+        ("cross-origin-embedder-policy".to_string(), "require-corp".to_string()),
+        ("cross-origin-opener-policy".to_string(), "same-origin".to_string()),
+        ("content-length".to_string(), body.len().to_string()),
+        (IC_CERTIFICATE_EXPRESSION_HEADER.to_string(), cel_expr),
+    ];
+    headers.extend(additional_headers);
+
+    HttpAssetResponse {
+        status_code: 200,
+        headers,
+        body: Cow::Borrowed(body),
+    }
 }
 
 // Encoding
