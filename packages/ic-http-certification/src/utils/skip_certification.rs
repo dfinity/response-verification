ad278b2456b7822a4283050abf9d4e39801da3d3
NathanosDev <contact@nathanos.dev>
2024-09-18 21:32:17 +0200

    docs(ic-http-certification): improve naming in example project

--- packages/ic-http-certification/src/utils/skip_certification.rs
+++ packages/ic-http-certification/src/utils/skip_certification.rs
@@ -1,4 +1,4 @@
-use super::add_certificate_header;
+use super::add_v2_certificate_header;
 use crate::{
     DefaultCelBuilder, Hash, HttpCertificationPath, HttpResponse,
     CERTIFICATE_EXPRESSION_HEADER_NAME,
@@ -26,6 +26,7 @@ use ic_representation_independent_hash::hash;
 ///
 /// let mut response = HttpResponse::builder().build();
 ///
+/// // this should normally be retrieved using `ic_cdk::api::data_certificate()`.
 /// let data_certificate = vec![1, 2, 3];
 ///
 /// add_skip_certification_header(data_certificate, &mut response);
@@ -45,8 +46,8 @@ use ic_representation_independent_hash::hash;
 /// );
 /// ```
 pub fn add_skip_certification_header(data_certificate: Vec<u8>, response: &mut HttpResponse) {
-    add_certificate_header(
-        data_certificate,
+    add_v2_certificate_header(
+        &data_certificate,
         response,
         &skip_certification_asset_tree(),
         &HttpCertificationPath::wildcard("").to_expr_path(),
@@ -68,7 +69,7 @@ pub fn add_skip_certification_header(data_certificate: Vec<u8>, response: &mut H
 ///
 /// let certified_data = skip_certification_certified_data();
 ///
-/// set_certified_date(&certified_data);
+/// set_certified_data(&certified_data);
 /// ```
 pub fn skip_certification_certified_data() -> Hash {
     skip_certification_asset_tree().digest()
@@ -85,4 +86,22 @@ fn skip_certification_asset_tree() -> HashTree {
         "http_expr",
         labeled("<*>", labeled(cel_expr_hash, leaf(vec![]))),
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_skip_certification_certified_data() {
+        let certified_data = skip_certification_certified_data();
+
+        assert_eq!(
+            certified_data,
+            [
+                85, 236, 195, 28, 62, 128, 71, 252, 21, 143, 32, 234, 10, 160, 96, 154, 172, 199,
+                181, 126, 6, 234, 64, 220, 65, 134, 2, 114, 167, 214, 66, 145
+            ]
+        );
+    }
 }
